71dd74776a719f380e942c0e64c0ce794f058392
Michael Pasimio <mvp36@georgetown.edu>
2026-03-19 23:40:39 -0400

    edited project tags

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,51 +3,51 @@ export const projects = [
         id: 1,
         slug: "cornucopia-college",
         title: "Cornucopia College",
-        type: "Social Vision & Strategy",
+        type: "Redesigning Education",
         image: "../../tech.jpg",
         description: "An imaginative design for a 'farm-to-table' micro-college.",
-        skillTags: ["social", "wellness"],
+        skillTags: ["curriculum", "design"],
         year: "2024",
-        tags_display: ["design", "social"]
+        tags_display: ["curriculum design", "design thinking"]
     },
     {
         id: 2,
         slug: "the-tennis-journey",
         title: "The Tennis Journey Workshop",
-        type: "Social Vision & Strategy",
+        type: "Teaching and Curriculum",
         image: "../../car-barn.jpg",
         description: "I delivered a workshop to address high burnout amongst high school athletes.",
-        skillTags: ["social"],
+        skillTags: ["curriculum", "teaching"],
         year: "2024",
-        tags_display: ["design", "facilitation", "consulting"]
+        tags_display: ["curriculum design", "teaching"],
     },
     {
         id: 3,
         slug: "community-university-model",
         title: "Community-University Model",
-        type: "Social Vision & Strategy",
+        type: "Redesigning Education",
         image: "../../social.jpg",
         description: "Designed a 'community-university' that centers the needs of the community and promotes democratic practice.",
-        skillTags: ["social", "design", "policy"],
+        skillTags: ["curriculum", "design"],
         year: "2024",
-        tags_display: ["design", "social", "policy"]
+        tags_display: ["curriculum design", "design thinking"]
     },
     {
         id: 4,
         slug: "teaching-english",
         title: "Teaching English in France",
-        type: "Social Vision & Strategy",
+        type: "Teaching and Curriculum",
         image: "../../social.jpg",
         description: "Developed English curriculum centered around games and reflection.",
-        skillTags: ["social", "curriculum design"],
+        skillTags: ["teaching", "curriculum"],
         year: "2022-2023",
-        tags_display: ["social", "teaching", "curriculum design"]
+        tags_display: ["teaching", "curriculum design"]
     },
     {
         id: 5,
         slug: "the-pivotal-network",
         title: "Pivotal Network Teacher R&D Community",
-        type: "Systems & Technology",
+        type: "Innovation in Complexity",
         image: "../../social.jpg",
         description: "Sustained community of 50 teacher-researchers and designed website.",
         skillTags: ["social", "systems"],
@@ -58,18 +58,18 @@ export const projects = [
         id: 6,
         slug: "rutgers-urban-planning",
         title: "Rutgers University Urban Planning",
-        type: "Social Vision & Strategy",
+        type: "Redesigning Education",
         image: "../../social.jpg",
         description: "analysis of Rutgers",
         skillTags: ["social"],
         year: "2025",
-        tags_display: ["social"]
+        tags_display: ["research"]
     },
     {
         id: 7,
         slug: "learning-employment-records",
         title: "Learning & Employment Records Analysis",
-        type: "Systems & Technology",
+        type: "Educational Technology",
         image: "../../social.jpg",
         description: "analysis of the emerging technology Learning and Employment Records (LERS)",
         skillTags: ["systems"],
@@ -79,12 +79,34 @@ export const projects = [
     {
         id: 8,
         slug: "ai-enrollment-management",
-        type: "Systems & Technology",
+        type: "Educational Technology",
         title: "AI and Enrollment Management",
         image: "../../social.jpg",
         description: "Analyzing the impact of AI and the enrollment management industry",
         skillTags: ["systems"],
         year: "2025",
         tags_display: ["tech", "policy"]
+    },
+    {
+        id: 9,
+        slug: "alumni-engagement-metrics",
+        type: "Innovation in Complexity",
+        title: "Alumni Engagement Metrics",
+        image: "../../social.jpg",
+        description: "Lorem ipsum dolor si je tape comme ca je pense que ca devien de plus en plus semblable au text de lorem ipsum",
+        skillTags: ["systems"],
+        year: "2025",
+        tags_display: ["tech", "policy"]
+    },
+    {
+        id: 10,
+        slug: "university-design-problem",
+        type: "Redesigning Education",
+        title: "The University as a Design Problem",
+        image: "../../social.jpg",
+        description: "Lorem ipsum dolor si je tape comme ca je pense que ca devien de plus en plus semblable au text de lorem ipsum",
+        skillTags: ["systems"],
+        year: "2025",
+        tags_display: ["design thinking", "curriculum design"]
     }
 ];
